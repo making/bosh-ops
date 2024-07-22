b70ceb700b9347cdc5d116725e09c20a7bf21080
Toshiaki Maki <makingx@gmail.com>
2024-07-22 15:13:46 +0900

    Use VARIABLE

--- install-bosh.sh
+++ install-bosh.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-wget -q https://github.com/cloudfoundry/bosh-cli/releases/download/v7.6.1/bosh-cli-7.6.1-linux-amd64
-chmod +x bosh-cli-7.6.1-linux-amd64
-sudo mv bosh-cli-7.6.1-linux-amd64 /usr/local/bin/bosh
+BOSH_VERSION=7.6.1
+
+wget -q https://github.com/cloudfoundry/bosh-cli/releases/download/v${BOSH_VERSION}/bosh-cli-${BOSH_VERSION}-linux-amd64
+chmod +x bosh-cli-${BOSH_VERSION}-linux-amd64
+sudo mv bosh-cli-${BOSH_VERSION}-linux-amd64 /usr/local/bin/bosh
 bosh -v
